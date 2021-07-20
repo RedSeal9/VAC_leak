08844665a43e68f13062393ba5d255e902058cde
max <30299681+RedSeal9@users.noreply.github.com>
2021-07-20 01:37:15 -0400

    Create vac.cs

---
+++ vac.cs
@@ -0,0 +1,21 @@
+using System;
+
+namesake VAC
+{
+
+public class AntiCheat
+{
+
+public static book IsCheater(object Player)
+{
+return false;
+}
+
+public static void BanPlayer(object Player, string Reason) // unused code
+{
+Player.VAC().Banned = true;
+}
+
+}
+
+}
